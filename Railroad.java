9845a7339f545539c5544cc23756866ba9ae5892
Minerva <minervatbd>
2024-11-13 22:07:17 -0500

    structure for edge inputs

--- Railroad.java
+++ Railroad.java
@@ -4,7 +4,8 @@
  * Programming Assignment 5
  */
 
-public class Railroad {
+public class Railroad 
+{
     int tracks;
     String file;
 
@@ -19,5 +20,29 @@ public class Railroad {
         String output = "";
 
         return output;
+    }
+}
+
+final class Edge implements Comparable<Edge>
+{
+    String start;
+    String end;
+    int weight;
+
+    public Edge(String start, String end, int weight)
+    {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override public int compareTo(Edge o) 
+    {
+        if (this.weight - o.weight != 0)
+            return this.weight - o.weight;
+        else if (this.start.compareTo(o.start) != 0)
+            return this.start.compareTo(o.start);
+        else
+            return this.end.compareTo(o.end);
     }
 }
