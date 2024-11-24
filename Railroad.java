9880f020893ccbe7e680ceb2a9774107c764c5b6
Minerva <minervatbd>
2024-11-24 14:42:39 -0500

    comments

--- Railroad.java
+++ Railroad.java
@@ -10,12 +10,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+// main class
 public class Railroad 
 {
     int trackNum;
     int stationNum;
-    Edge[] tracks;
-    ArrayList<String> stations;
+    Edge[] tracks; // all edges
+    ArrayList<String> stations; // all vertices
 
     public Railroad(int n, String fName)
     {
@@ -30,10 +31,13 @@ public class Railroad
 
             Scanner fScan = new Scanner(inFile);
 
+            // populate tracks and stations
             for (int x = 0; x < this.trackNum; x++)
             {
+                // always add to tracks list
                 this.tracks[x] = new Edge(fScan.next(), fScan.next(), Integer.parseInt(fScan.next()));
 
+                // add to stations if any new
                 if (!stations.contains(this.tracks[x].start))
                 {
                     stations.add(this.tracks[x].start);
@@ -45,12 +49,14 @@ public class Railroad
                     stationNum++;
                 }
             }
-
+            
+            // now tracks can be sorted
             Arrays.sort(this.tracks);
 
             fScan.close();
         }
 
+        // input invalid case
         catch (FileNotFoundException e)
         {
             System.err.println("Input file not found!");
@@ -58,51 +64,56 @@ public class Railroad
         }        
     }
 
+    // kruskal implementation, edges already sorted
     public String buildRailroad()
     {
+        // minimum spanning tree edge list
         Edge[] mst = new Edge[trackNum - 1];
 
+        // disjoint set with stationnum entries
         DisjointSetImproved disj = new DisjointSetImproved(stationNum);
 
         String output = "";
 
-        int x, y;
-
-        int mstCount = 0;
+        // index ints
+        int x, y, mstCount = 0;
 
         int totalWeight = 0;
 
+        // kruskal loop
         for (int c = 0; c < trackNum; c++)
         {
             x = stations.indexOf(tracks[c].start);
             y = stations.indexOf(tracks[c].end);
+            // check for a connection
             if (disj.find(x) != disj.find(y))
             {
+                // if not, connect them
                 disj.union(x, y);
                 mst[mstCount] = tracks[c];
-                mstCount++;
+                mstCount++; // and increment
+
+                // for output
                 totalWeight += tracks[c].weight;
                 output += tracks[c].toString();
             }
         }
 
+        // last part of output
         output += "The cost of the railroad is $" + Integer.toString(totalWeight) + ".";
 
         return output;
     }
-
-    int getIndex(String station)
-    {
-        return stations.indexOf(station);
-    }
 }
 
+// for sorting and string output
 final class Edge implements Comparable<Edge>
 {
     String start;
     String end;
     int weight;
 
+    // constructor
     public Edge(String start, String end, int weight)
     {
         this.start = start;
@@ -110,6 +121,7 @@ final class Edge implements Comparable<Edge>
         this.weight = weight;
     }
 
+    // sorting interface, based on weight
     @Override public int compareTo(Edge o) 
     {
         if (this.weight - o.weight != 0)
@@ -120,8 +132,10 @@ final class Edge implements Comparable<Edge>
             return this.end.compareTo(o.end);
     }
 
+    // string output
     @Override
     public String toString() {
+        // formatted lexically
         if (start.compareTo(end) <= 0)
             return start + "---" + end + "\t$" + Integer.toString(weight) + "\n";
         else
