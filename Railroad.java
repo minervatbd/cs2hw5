d7f6acb3ecb8ae33b21400e5486ff0f8af924bcf
Minerva <minervatbd>
2024-11-25 19:01:54 -0500

    nested classes just in case

--- Railroad.java
+++ Railroad.java
@@ -104,41 +104,108 @@ public class Railroad
 
         return output;
     }
-}
 
-// for sorting and string output
-final class Edge implements Comparable<Edge>
-{
-    String start;
-    String end;
-    int weight;
-
-    // constructor
-    public Edge(String start, String end, int weight)
+    // edge class for sorting and output
+    class Edge implements Comparable<Edge>
     {
-        this.start = start;
-        this.end = end;
-        this.weight = weight;
+        String start;
+        String end;
+        int weight;
+
+        // constructor
+        public Edge(String start, String end, int weight)
+        {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        // sorting interface, based on weight
+        @Override public int compareTo(Edge o) 
+        {
+            if (this.weight - o.weight != 0)
+                return this.weight - o.weight;
+            else if (this.start.compareTo(o.start) != 0)
+                return this.start.compareTo(o.start);
+            else
+                return this.end.compareTo(o.end);
+        }
+
+        // string output
+        @Override
+        public String toString() {
+            // formatted lexically
+            if (start.compareTo(end) <= 0)
+                return start + "---" + end + "\t$" + Integer.toString(weight) + "\n";
+            else
+                return end + "---" + start + "\t$" + Integer.toString(weight) + "\n";
+        }
     }
 
-    // sorting interface, based on weight
-    @Override public int compareTo(Edge o) 
+    // from given DisjointSetImproved.java
+    class DisjointSetImproved
     {
-        if (this.weight - o.weight != 0)
-            return this.weight - o.weight;
-        else if (this.start.compareTo(o.start) != 0)
-            return this.start.compareTo(o.start);
-        else
-            return this.end.compareTo(o.end);
-    }
-
-    // string output
-    @Override
-    public String toString() {
-        // formatted lexically
-        if (start.compareTo(end) <= 0)
-            return start + "---" + end + "\t$" + Integer.toString(weight) + "\n";
-        else
-            return end + "---" + start + "\t$" + Integer.toString(weight) + "\n";
+        int [] rank;
+        int [] parent;
+        int n;
+        
+        public DisjointSetImproved(int n)
+        {
+            rank = new int[n];
+            parent = new int[n];
+            this.n = n;
+            makeSet();
+        }
+        
+        // Creates n sets, single item per
+        public void makeSet()
+        {
+            for (int i = 0; i < n; i++) 
+            {
+                parent[i] = i;
+            }
+        }
+        
+        //path compression
+        public int find(int x)
+        {
+            if (parent[x] != x) 
+            {
+                parent[x] = find(parent[x]);
+            }
+    
+            return parent[x];
+        }
+        
+        //union by rank
+        public void union(int x, int y)
+        {
+            int xRoot = find(x), yRoot = find(y);
+    
+            if (xRoot == yRoot)
+                return;
+            
+            if (rank[xRoot] < rank[yRoot])
+                parent[xRoot] = yRoot;
+            
+            else if (rank[yRoot] < rank[xRoot])
+                parent[yRoot] = xRoot;
+            else 
+            {
+                parent[yRoot] = xRoot;
+                rank[xRoot] = rank[xRoot] + 1;
+            }
+        }
+        
+        
+        public static void printSets(int[] universe, DisjointSetImproved ds)
+        {
+            for (int i: universe) 
+            {
+                System.out.print(ds.find(i) + " ");
+            }
+    
+            System.out.println();
+        }
     }
 }
